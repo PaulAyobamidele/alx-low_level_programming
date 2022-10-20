741219894e5f45e23ecf472ad430917bf6028e0e
PaulAyobamidele <gethelp.revayah@gmail.com>
2022-10-20 14:46:36 -0700

    five

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,16 @@ void more_numbers(void)
 {
 	int i, ro;
 
-	for (ro = 0; ro <= 9; i++)
+	for (ro = 0; ro < 10; ro++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			for (i = 0; i <= 14; i++)
+			if (i >= 10)
 			{
-				if (i > 9)
-				{
-					_putchar((i / 10) + '0');
-				}
-				_putchar((i % 10) + '0');
+				_putchar((i / 10) + '0');
 			}
-			_putchar('\n');
+			_putchar((i % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
