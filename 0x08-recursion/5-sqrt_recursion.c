180e07620c16aedff05373ce39c1bb927ff48aa4
PaulAyobamidele <gethelp.revayah@gmail.com>
2022-11-02 14:14:04 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+nclude "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * find_sqrt - Finds the natural square root
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural
+ * @n: The number to return the sqaure root of
+ *
+ * Return: If n has a natural square root
+ * If n does not have a natural square root - -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
+}
