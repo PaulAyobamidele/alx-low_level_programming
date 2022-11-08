d8b2744b301d8e5b2a05e42eb0209a503e575b06
PaulAyobamidele <gethelp.revayah@gmail.com>
2022-11-08 09:08:25 -0800

    4-free_grid.c refix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
+#include "holberton.h"
+
 
 /**
- * free_grid - Fress a 2d
- * @grid: The 2d array
- * @height: The height of grid
+ * free_grid - fress the memory
+ * created by alloc_grid()
+ * @grid: grid to free
+ * @height: height of the grid
+ *
  */
 void free_grid(int **grid, int height)
 {
-	int index;
+	int i;
 
-	for (index = 0; index < height; index++)
-		free(grid[index]);
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
 	free(grid);
 }
